9b81a7ccb23791ca94d4f0758ee8d57f2824fb39
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-05-08 09:28:43 +0300

    feat: full delivery status items

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,13 +1,13 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next:Node = None
-        #Optional, will not always be used based on the type of the Linked List.
-        self.previous:Node = None  
+        self.next: Node = None
+        # Optional, will not always be used based on the type of the Linked List.
+        self.previous: Node = None
 
     def __repr__(self):
         return f"Node({self.data})"
-    
+
 
 class SinglyLinkedList:
     def __init__(self):
@@ -21,7 +21,7 @@ class SinglyLinkedList:
             representative += f"{node} -> "
             node = node.next
         return representative.rstrip(" -> ")
-    
+
     def traverse(self, data):
         """Traverses the list to find a node with the given data"""
         current_node = self.head
@@ -30,7 +30,7 @@ class SinglyLinkedList:
                 return current_node
             current_node = current_node.next
         return None
-    
+
     def insert_head(self, data):
         """Inserts data at the head"""
         new_node = Node(data)
@@ -38,11 +38,11 @@ class SinglyLinkedList:
             self.head = new_node
             self.tail = new_node
             return self.head
-        
+
         new_node.next = self.head
         self.head = new_node
         return new_node
-    
+
     def insert_end(self, data):
         """Inserts data at the tail"""
         new_node = Node(data)
@@ -50,33 +50,33 @@ class SinglyLinkedList:
             self.head = new_node
             self.tail = new_node
             return self.tail
-        
+
         self.tail.next = new_node
         self.tail = new_node
         return new_node
-    
+
     def insert_after_node(self, after_node_data, data):
         """Inserts data after a node with the given data"""
         after_node = self.traverse(after_node_data)
         if after_node is None:
             return None
-            
+
         new_node = Node(data)
-        
+
         # If inserting after the tail
         if after_node == self.tail:
             return self.insert_end(data)
-            
+
         new_node.next = after_node.next
         after_node.next = new_node
-        
+
         return new_node
-    
+
     def delete_node(self, node_data):
         """Deletes the node with the given data"""
         if not self.head:
             return None
-            
+
         # If deleting the head
         if self.head.data == node_data:
             self.head = self.head.next
@@ -84,26 +84,28 @@ class SinglyLinkedList:
             if not self.head:
                 self.tail = None
             return
-        
+
         # Find the node before the one to delete
         current = self.head
         while current.next and current.next.data != node_data:
             current = current.next
-            
+
         # If the node to delete was not found
         if not current.next:
             return None
-            
+
         # If deleting the tail
         if current.next == self.tail:
             self.tail = current
-            
+
         # Delete the node
         current.next = current.next.next
+
+
 class DoublyLinkedList:
     def __init__(self):
-        self.head:Node = None
-        self.tail:Node = None
+        self.head: Node = None
+        self.tail: Node = None
 
     def __repr__(self):
         representative = ""
@@ -112,7 +114,7 @@ class DoublyLinkedList:
             representative += f"{node} <-> "
             node = node.next
         return representative.rstrip(" <-> ")
-    
+
     def traverse_forward(self, data):
         current_node = self.head
         while current_node:
@@ -121,7 +123,7 @@ class DoublyLinkedList:
                 return current_node
             current_node = current_node.next
         return None
-    
+
     def traverse_backward(self, data):
         current_node = self.tail
         while current_node:
@@ -129,7 +131,7 @@ class DoublyLinkedList:
                 return current_node
             current_node = current_node.previous
         return None
-    
+
     def insert_head(self, data):
         """Inserts data at the head"""
         new_node = Node(data)
@@ -137,13 +139,13 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
             return self.head
-        
+
         old_head = self.head
         new_node.next = old_head
         old_head.previous = new_node
         self.head = new_node
         return new_node
-    
+
     def insert_end(self, data):
         """Inserts data at the tail"""
         new_node = Node(data)
@@ -151,34 +153,34 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
             return self.tail
-        
+
         tail_node = self.tail
         tail_node.next = new_node
-        new_node.previous = tail_node 
+        new_node.previous = tail_node
         self.tail = new_node
         return new_node
-    
+
     def insert_after_node(self, after_node_data, data):
         after_node = self.traverse_forward(after_node_data)
         if after_node is None:
             # print(f"Node with data {after_node_data} not found")
             return None
-            
+
         new_node = Node(data)
-        
+
         # If inserting after the tail
         if after_node == self.tail:
             return self.insert_end(data)
-            
+
         new_node.next = after_node.next
         new_node.previous = after_node
-        
+
         if after_node.next:
             after_node.next.previous = new_node
         after_node.next = new_node
-        
+
         return new_node
-    
+
     def delete_node_forward(self, node_delete_data):
         """deletes item by traversing forward"""
         delete_node = self.traverse_forward(node_delete_data)
@@ -189,11 +191,11 @@ class DoublyLinkedList:
         delete_node = self.traverse_backward(node_delete_data)
         self.delete_node(delete_node)
 
-    def delete_node(self,delete_node:Node):
+    def delete_node(self, delete_node: Node):
         if not delete_node:
             # print(f"Node with data {node_delete_data} not found")
             return None
-            
+
         # If deleting the head
         if delete_node == self.head:
             self.head = delete_node.next
@@ -203,16 +205,17 @@ class DoublyLinkedList:
                 # If list becomes empty
                 self.tail = None
             return
-            
+
         # If deleting the tail
         if delete_node == self.tail:
             self.tail = delete_node.previous
             self.tail.next = None
             return
-            
+
         # If deleting a middle node
         delete_node.previous.next = delete_node.next
         delete_node.next.previous = delete_node.previous
+
 
 class CircularLinkedList:
     def __init__(self):
@@ -222,45 +225,53 @@ class CircularLinkedList:
     def __repr__(self):
         if not self.head:
             return ""
-            
+
         representative = ""
         node = self.head
-        
+
         # First node
         representative += f"{node} -> "
         node = node.next
-        
+
         # Rest of the nodes until we reach head again
         while node and node != self.head:
             representative += f"{node} -> "
             node = node.next
-            
+
         # Show the circular nature
         representative += "..."
         return representative
-    
+
     def traverse(self, data):
         """Traverses the list to find a node with the given data"""
         if not self.head:
             return None
-            
+
         current_node = self.head
-        
+
         # Check the first node
         if current_node.data == data:
             return current_node
-            
+
         current_node = current_node.next
-        
+
         # Check the rest of the nodes until we reach head again
         while current_node and current_node != self.head:
             if current_node.data == data:
                 return current_node
             current_node = current_node.next
-            
+
         # If we reach here, the node was not found
         return None
-    
+
+    def traverse_full_cycle(self):
+        if not self.head:
+            return None
+        current_node = self.head
+        while current_node:
+            yield current_node
+            current_node = current_node.next
+
     def insert_head(self, data):
         """Inserts data at the head"""
         new_node = Node(data)
@@ -270,90 +281,74 @@ class CircularLinkedList:
             # Make it circular
             new_node.next = new_node
             return new_node
-        
+
         # Connect new node to head and tail
         new_node.next = self.head
         self.tail.next = new_node
         self.head = new_node
-        
+
         return new_node
-    
+
     def insert_end(self, data):
         """Inserts data at the tail"""
         if not self.head:
             return self.insert_head(data)
-            
+
         new_node = Node(data)
-        
+
         # Connect new node to head and update tail
         new_node.next = self.head
         self.tail.next = new_node
         self.tail = new_node
-        
+
         return new_node
-    
+
     def insert_after_node(self, after_node_data, data):
         """Inserts data after a node with the given data"""
         after_node = self.traverse(after_node_data)
         if not after_node:
             return None
-            
+
         new_node = Node(data)
-        
+
         # If inserting after the tail
         if after_node == self.tail:
             return self.insert_end(data)
-            
+
         new_node.next = after_node.next
         after_node.next = new_node
-        
+
         return new_node
-    
+
     def delete_node(self, node_data):
         """Deletes the node with the given data"""
         if not self.head:
             return None
-            
         # Case 1: Only one node in the list
         if self.head == self.tail:
             if self.head.data == node_data:
                 self.head = None
                 self.tail = None
             return
-            
+
         # Case 2: Deleting the head
         if self.head.data == node_data:
             self.head = self.head.next
             self.tail.next = self.head
             return
-            
+
         # Case 3: Deleting a node that's not the head
         current = self.head
         while current.next != self.head and current.next.data != node_data:
             current = current.next
-            
+
         # If the node was not found
         if current.next == self.head:
             return None
-            
+
         # If deleting the tail
         if current.next == self.tail:
             self.tail = current
-            
+
         # Delete the node
         current.next = current.next.next
-        
-
-if __name__ == "__main__":
-    doubly_list = CircularLinkedList()
-    doubly_list.insert_head(1)
-    print(f"{doubly_list}")
-    
-    doubly_list.insert_end(3)
-    print(f"{doubly_list}")
-    
-    node_2 = doubly_list.insert_after_node(1, 2)
-    print(f"{doubly_list}")
-    
-    doubly_list.delete_node(node_2.data)
-    print(f"{doubly_list}")
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from data_structures.stack import StackLinkedList
 from data_structures.queue import QueueLinkedBased
+from data_structures.linked_list import CircularLinkedList, Node
+import random
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     wish_cart = StackLinkedList()
     # If a user puts an item to purchase card.
     wish_cart.push("Book")
     print(f"User wish cart: {wish_cart}")
-    wish_cart.push("TV")   
+    wish_cart.push("TV")
     print(f"User wish cart: {wish_cart}")
     # The user wants to add another item.
     wish_cart.push("Ball")
@@ -20,11 +21,24 @@ if __name__=="__main__":
         print(f"Registered item: {item_to_process.data} to be delivered to the user.")
         order_process.enqueue(item_to_process)
         item_to_process = wish_cart.pop()
-        
-    print("\n")
-    # Now time to deliver
-    item_to_deliver = order_process.dequeue()
-    while item_to_deliver:
-        print(f"Dilvered item: {item_to_deliver}")
-        item_to_deliver = order_process.dequeue()
 
+    print("\n")
+    delivery_status = CircularLinkedList()
+    # Now time to deliver
+    item_to_deliver: Node = order_process.dequeue()
+    while item_to_deliver:
+        delivery_status.insert_head(item_to_deliver.data)
+        if random.randint(1, 10) < 5:  # 50% chance delivery
+            delivery_status.delete_node(item_to_deliver.data)
+            print(f"Dilvered item: {item_to_deliver.data}")
+        item_to_deliver = order_process.dequeue()
+    not_delivered = next(delivery_status.traverse_full_cycle())
+    while not_delivered:
+        if random.randint(1, 10) < 9:  # 90% chance delivery
+            print(f"Deliverd Item: {not_delivered.data}")
+            delivery_status.delete_node(not_delivered.data)
+            try:
+                not_delivered = next(delivery_status.traverse_full_cycle())
+            except:
+                print(f"Delivered all items....")
+                not_delivered = None
